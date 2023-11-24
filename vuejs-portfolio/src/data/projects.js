926f0f9590b52aa7481e30b47a03cb357b71c757
Kuesung Park <gueit214@naver.com>
2023-11-25 08:42:53 +0900

    feat : init project

---
+++ vuejs-portfolio/src/data/projects.js
@@ -0,0 +1,41 @@
+// Projects data
+const projects = [
+  {
+    id: 1,
+    title: "Gloddy",
+    category: "웹 애플리케이션",
+    img: require("@/assets/images/web-project-2.jpg"),
+  },
+  {
+    id: 2,
+    title: "2023 영암 월출산국화축제",
+    category: "Web Application",
+    img: require("@/assets/images/mobile-project-2.jpg"),
+  },
+  {
+    id: 3,
+    title: "나만의 포트폴리오 개발",
+    category: "Web Application",
+    img: require("@/assets/images/ui-project-1.jpg"),
+  },
+  {
+    id: 4,
+    title: "Atties",
+    category: "Mobile Application",
+    img: require("@/assets/images/ui-project-2.jpg"),
+  },
+  {
+    id: 5,
+    title: "React Social App",
+    category: "UI/UX Design",
+    img: require("@/assets/images/mobile-project-1.jpg"),
+  },
+  {
+    id: 6,
+    title: "Apple Design System",
+    category: "Branding",
+    img: require("@/assets/images/web-project-1.jpg"),
+  },
+];
+
+export default projects;
---
+++ vuejs-portfolio/src/data/socialLinks.js
@@ -0,0 +1,32 @@
+export const socialLinks = [
+  {
+    id: 1,
+    name: "Website",
+    icon: "globe",
+    url: "https://www.guesung.me/",
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    icon: "github",
+    url: "https://github.com/realguesung",
+  },
+  {
+    id: 3,
+    name: "Twitter",
+    icon: "twitter",
+    url: "https://twitter.com/realguesung",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+    icon: "linkedin",
+    url: "https://www.linkedin.com/in/realguesung",
+  },
+  {
+    id: 5,
+    name: "YouTube",
+    icon: "youtube",
+    url: "https://www.youtube.com/c/realguesung",
+  },
+];
